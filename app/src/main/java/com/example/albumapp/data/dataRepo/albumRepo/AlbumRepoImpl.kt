f30747fe3fa64b10f5fdc0d43b34a8448729773c
Heba-Alii <hebaaliali18@gmail.com>
2024-03-27 22:25:26 +0200

    edits

--- app/src/main/java/com/example/albumapp/data/dataRepo/albumRepo/AlbumRepoImpl.kt
+++ app/src/main/java/com/example/albumapp/data/dataRepo/albumRepo/AlbumRepoImpl.kt
@@ -7,8 +7,6 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOn
 
 class AlbumRepoImpl(private val apiService: ApiService) : AlbumRepo {
-
-
     override suspend fun getAlbumFromRemote(id: Int) = flow {
         emit(apiService.getAlbums(id))
     }.flowOn(Dispatchers.IO)
